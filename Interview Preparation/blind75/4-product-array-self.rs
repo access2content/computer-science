b85a41207e67bc0edc128bd4d00ffc9a6435c3f2
Vivek Agrawal <access2content@gmail.com>
2023-11-14 17:11:13 +0530

    feat(blind75): product array except self

---
+++ Interview Preparation/blind75/4-product-array-self.rs
@@ -0,0 +1,23 @@
+pub fn product_except_self(nums: Vec<i32>) -> Vec<i32> {
+    let mut answer = vec![1; nums.len()];
+
+    //  Loop over the nums once to calculate prefix product
+    for i in 1..nums.len() {
+        answer[i] = answer[i - 1] * nums[i - 1];
+    }
+
+    //  Loop in the reverse direction
+    let mut temp = 1;
+    for i in (0..nums.len() - 1).rev() {
+        temp *= nums[i + 1];
+        answer[i] *= temp;
+    }
+
+    return answer;
+}
+
+fn main() {
+    let input: Vec<i32> = vec![1, 2, 3, 4];
+    let output = product_except_self(input);
+    println!("Output: {:?}", output);
+}
